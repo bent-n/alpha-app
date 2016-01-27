e2ee2fbc3e797e8c5fa2c98b61b6b7e2fecbd87c
Benton <thebentonparrish@gmail.com>
2016-01-27 15:15:37 -0500

    signup users ui

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
 
   root 'welcome#home'
-
+  
   get 'welcome/home' => 'welcome#home'
   get '/about' => 'welcome#about'
-  resources :articles
+  get 'signup' => 'users#new'
+  resources :articles 
+  resources :users, except: [:new]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
