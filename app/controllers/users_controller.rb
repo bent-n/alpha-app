5146623af07113d46a3413f4d244d23e0a1b2fe2
Benton <thebentonparrish@gmail.com>
2016-02-01 14:50:13 -0500

    admin delete permissions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
   
-  before_action :set_user, only: [:edit, :update, :show, :post_index]
+  before_action :set_user, only: [:edit, :update, :show, :destroy, :post_index]
   before_action :require_user, only: [:edit, :update, :destroy]
   before_action :require_same_user, only: [:edit, :update, :destroy]
+  before_action :require_admin, only: :destroy
 
   def new
     @user=User.new
@@ -39,6 +40,14 @@ def show
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
 end
 
+def destroy
+  u_name = @user.username
+  a_count = @user.articles.count
+  @user.destroy
+  flash[:danger] = "User: #{u_name} and #{pluralize(a_count, "Article")} deleted"
+  redirect_to users_path
+end
+
 def post_index
    @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
    render :layout => false , :except => :view
@@ -57,4 +66,11 @@ end
         redirect_to root_path
       end
     end
+    def require_admin
+      if logged_in? && !current_user.admin?
+        flash[:danger] = "Admin permissions required"
+        redirect_to root_path
+      end
+    end
+    
 end
